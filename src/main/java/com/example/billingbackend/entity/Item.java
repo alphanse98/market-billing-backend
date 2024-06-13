10d092919a61858b9e3ae3b515869579db93ce5b
guna <gunario007@gmail.com>
2024-06-13 12:46:53 +0530

    billing item controller and item validation

--- src/main/java/com/example/billingbackend/entity/Item.java
+++ src/main/java/com/example/billingbackend/entity/Item.java
@@ -1,6 +1,8 @@
 package com.example.billingbackend.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,20 +22,27 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
     @Column(nullable = false)
     private String businessID;
 
+    @NotEmpty
+    @Size(max = 15, message = "item name should have atmost 15 characters")
     @Column(nullable = false)
     private String itemName;
 
+    @NotEmpty
     @Column(nullable = false)
     private String itemImg;
 
+    @NotEmpty
     @Column(nullable = false)
     private Double itemPrice;
 
+    @NotEmpty
     private boolean isActive;
 
+    @NotEmpty
     @Column(nullable = true) // Setting nullable to true makes the column optional
     private Date createDate;
 }
